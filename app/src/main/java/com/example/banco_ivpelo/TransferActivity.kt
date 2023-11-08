68ee44328164e3aabcf42cb38ad5c4efc8ca96dc
ivan <ivan14valla@gmail.com>
2023-11-08 14:15:40 +0100

    T5Actividad2

--- app/src/main/java/com/example/banco_ivpelo/TransferActivity.kt
+++ app/src/main/java/com/example/banco_ivpelo/TransferActivity.kt
@@ -2,7 +2,9 @@ package com.example.banco_ivpelo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.banco_ivpelo.databinding.ActivityMain2Binding
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.banco_ivpelo.databinding.ActivityTransferBinding
 
 class TransferActivity : AppCompatActivity() {
@@ -22,7 +24,63 @@ class TransferActivity : AppCompatActivity() {
             "IT60-X054-2811-10-1000000123456"
         )
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val opcionesMonedas = listOf(
+            "€", "$"
+        )
+
+
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                binding.radioCuentaPropia.id -> {
+                    binding.cmbCuentaDestino.visibility = View.VISIBLE
+                    binding.editTextCuentaDestino.visibility = View.GONE
+                    binding.textView3
+                }
+
+                binding.radioOtraOpcion.id -> {
+                    binding.cmbCuentaDestino.visibility = View.GONE
+                    binding.editTextCuentaDestino.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        val adapterIban = ArrayAdapter(this, android.R.layout.simple_spinner_item, opcionesIBAN)
+        val adapterMoneda =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, opcionesMonedas)
+
+        adapterIban.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapterMoneda.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.cmbOpciones3.adapter = adapterMoneda
+        binding.cmbCuentaOrigen.adapter = adapterIban
+        binding.cmbCuentaDestino.adapter = adapterIban
+
+        binding.btnCancelar.setOnClickListener {
+            binding.editTextCuentaDestino.text.clear()
+            binding.editTextCantidad.text.clear()
+            binding.checkboxJustificante.isChecked = false
+        }
+
+        binding.btnEnviar.setOnClickListener {
+            val cuentaOrigen = binding.cmbCuentaOrigen.selectedItem.toString()
+            var cuentaDestino = ""
+
+            if (binding.radioCuentaPropia.isChecked) {
+                cuentaDestino = binding.cmbCuentaOrigen.selectedItem.toString()
+            } else {
+                cuentaDestino = binding.editTextCuentaDestino.text.toString()
+            }
+
+            val moneda = binding.cmbOpciones3.selectedItem.toString()
+            val enviarJustificante = binding.checkboxJustificante.isChecked
+            val importe = binding.editTextCantidad.text.toString()
+
+            val mensaje =
+                "Cuenta Origen: $cuentaOrigen\nCuenta Destino: $cuentaDestino\nImporte: $importe $moneda\nEnviar Justificante: $enviarJustificante"
+
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 
